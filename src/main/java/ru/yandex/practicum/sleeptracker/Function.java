692c5f66ae48542f19e47ae9c33d5e9c0583f21c
Roman Savosin <savosin_33@mail.ru>
2026-05-22 12:48:32 +0300

    Добавил пустую строку в Function

--- src/main/java/ru/yandex/practicum/sleeptracker/Function.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/Function.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.sleeptracker;
 import java.util.List;
 
 public interface Function {
+
     int calculate(List<SleepingSession> sessions);
     String getDescription();
 }
